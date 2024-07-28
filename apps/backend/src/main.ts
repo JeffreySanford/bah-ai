db44e935753d3638beee96c1df0a2cd7dcea28c8
Jeffrey Sanford <jeffreysanford@gmail.com>
2024-07-28 04:33:22 -0500

    chore: Refactor main.ts for improved code structure and logging

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,23 +1,26 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
 import { setupSwagger } from './swaggerSetup';
+import { Logger } from '@nestjs/common';
+import { from } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+
+  from(NestFactory.create(AppModule)).pipe(
+    tap(app => {
+      app.setGlobalPrefix(globalPrefix);
+      setupSwagger(app);
+    }),
+    switchMap(app => from(app.listen(port))),
+    tap(() => {
+      Logger.log(
+        `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+      );
+    })
+  ).subscribe();
 }
 
 bootstrap();
