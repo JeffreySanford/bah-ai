102074364ec87ec53112c59d9d90b2828253551b
Jeffrey Sanford <jeffreysanford@gmail.com>
2024-07-28 16:54:25 -0500

    chore: Add HttpClientTestingModule to stock-movers component spec

--- apps/frontend/src/app/stock-movers/stock-movers.component.spec.ts
+++ apps/frontend/src/app/stock-movers/stock-movers.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { StockMoversComponent } from './stock-movers.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('StockMoversComponent', () => {
   let component: StockMoversComponent;
@@ -8,11 +9,12 @@ describe('StockMoversComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [StockMoversComponent],
+      imports: [HttpClientTestingModule], // Ensure HttpClientTestingModule is imported
     }).compileComponents();
 
     fixture = TestBed.createComponent(StockMoversComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    fixture.detectChanges(); // Ensure detectChanges is called after component instance creation
   });
 
   it('should create', () => {
