42acf63f14cd8afe216de2bbc9da6b2d19886527
Jeffrey Sanford <jeffreysanford@gmail.com>
2024-07-30 10:32:27 -0500

    chore: Update FinnhubController to include getStockData endpoint

--- apps/backend/src/app/stock/finnhub/finnhub.controller.ts
+++ apps/backend/src/app/stock/finnhub/finnhub.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { FinnhubService } from './finnhub.service';
 
 @Controller('finnhub')
-export class FinnhubController {}
+export class FinnhubController {
+  constructor(private readonly finnhubService: FinnhubService) {}
+
+  @Get('stock/:symbol')
+  getStockData(@Param('symbol') symbol: string): void {
+	this.finnhubService.getStockData(symbol).subscribe(data => {
+	  console.log(data);
+	});
+  }
+}
